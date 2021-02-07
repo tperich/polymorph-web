ab93b71071bf6b84d24376d9c832b5d5f5454398
Tomislav Peric <toca91@gmail.com>
2021-02-07 13:32:46 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php header("X-Powered-By: PHP7.2 or Python 3.4 or Ruby 2.5 we are not sure"); ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Polymorph</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="static/css/style.css?c=ooGha6do"/>
+		<link rel="stylesheet" href="static/css/animate.min.css?c=ooGha6do"/>
+		<link rel="shortcut icon" type="image/png" href="static/img/favicon.png?c=ooGha6do"/>
+	</head>
+	<body>
+		<canvas id="canvas" class="animated lightSpeedIn"></canvas>
+
+		<div id="box" class="animated fadeInDown">
+			<img class="logo" src="static/img/r3v2.png?c=ooGha6do">
+			<p class="text">DevOps services</p>
+			<p class="text mail">office@polymorph.rs</p>
+		</div>
+		
+		<script src="static/js/app.js?c=ooGha6do"></script>
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-53008476-2"></script>
+
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag() { dataLayer.push(arguments); }
+			gtag('js', new Date());
+      gtag('config', 'UA-53008476-2');
+		</script>
+
+	</body>
+</html>
